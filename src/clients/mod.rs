924bc3826d864f1fbc69b352d90ef1e5b334cef9
AonGoltzCrank <Alon.goltzman@gmail.com>
2024-01-07 21:50:20 +0200

    chore(clients): :construction: added macros for easier get and post operations

--- src/clients/mod.rs
+++ src/clients/mod.rs
@@ -1,3 +1,66 @@
 mod errors;
 pub mod indexer_client;
 pub mod indexer_client_types;
+
+
+
+// ========================================================
+// Traits and macros for easier conversion for get/post
+// ========================================================
+
+trait CamelCaseify {
+    fn to_camel_case(&self) -> Self;
+}
+
+impl CamelCaseify for String {
+    fn to_camel_case(&self) -> Self {
+        let mut convert_next = false;
+        self.chars()
+            .map(|c| {
+                if c == '_' {
+                    convert_next = true;
+                    return '\0';
+                }
+
+                if convert_next {
+                    convert_next = false;
+                    c.to_ascii_uppercase()
+                } else {
+                    c
+                }
+            })
+            .filter(|c| *c != '\0')
+            .collect()
+    }
+}
+
+#[macro_export]
+macro_rules! arg_to_tuple {
+    ($exp:expr) => {
+        (
+            stringify!($exp).to_string().to_camel_case(),
+            Some($exp.to_string()),
+        )
+    };
+}
+
+#[macro_export]
+macro_rules! option_to_tuple {
+    ($exp:expr) => {
+        (
+            stringify!($exp).to_string().to_camel_case(),
+            option_t_to_string_option!($exp),
+        )
+    };
+}
+
+#[macro_export]
+macro_rules! option_t_to_string_option {
+    ($exp:expr) => {
+        if let Some(e) = $exp {
+            Some(e.to_string())
+        } else {
+            None
+        }
+    };
+}
